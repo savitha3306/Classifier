a6faf9ef739bc9aea63a65a0c0a224b4b987d800
Savitha P <129480387+savitha3306@users.noreply.github.com>
2023-05-02 14:35:21 +0530

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -1,32 +1,34 @@
 import pandas as pd
 import streamlit as st
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import classification_report
-from sklearn.feature_extraction.text import CountVectorizer
 import pickle
-bbc_text = pd.read_csv('bbc-text.txt')
-bbc_text=bbc_text.rename(columns = {'text': 'News_Headline'}, inplace = False)
-bbc_text.head()
-bbc_text.category = bbc_text.category.map({'tech':0, 'business':1, 'sport':2, 'entertainment':3, 'politics':4})
-bbc_text.category.unique()
+bbc_text = pd.read_csv("bbc-text.txt")
+bbc_text = bbc_text.rename(columns = {"text":"News_Headline"}, inplace = False)
+bbc_text.category = bbc_text.category.map({"tech":0,"business":1,"sport":2,"entertainment":3,"politics":4})
+from sklearn.model_selection import train_test_split
 X = bbc_text.News_Headline
 y = bbc_text.category
+#split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.6, random_state = 1)
-vector = CountVectorizer(stop_words = 'english',lowercase=False)
+from sklearn.feature_extraction.text import CountVectorizer
+vector = CountVectorizer(stop_words = "english", lowercase = False)
+#fit the vectorizer on the training data
 vector.fit(X_train)
-vector.vocabulary_
 X_transformed = vector.transform(X_train)
 X_transformed.toarray()
+# for text data
 X_test_transformed = vector.transform(X_test)
+from sklearn.naive_bayes import MultinomialNB
 naivebayes = MultinomialNB()
 naivebayes.fit(X_transformed, y_train)
-headline1 = ['Portugal crash out of FIFA World Cup 2022, Ronaldo in tears']
-vec = vector.transform(headline1).toarray()
-headline1 = ['There will be recession throughout the world as predicted by world bank']
-vec = vector.transform(headline1).toarray()
+from sklearn.metrics import classification_report
+print(classification_report(naivebayes.predict(X_test_transformed), y_test))
+import pickle
 saved_model = pickle.dumps(naivebayes)
 s = pickle.loads(saved_model)
-headline1 = ['There will be recession throughout the world as predicted by world bank']
-vec = vector.transform(headline1).toarray()
-s.predict(vec)[0]
+
+st.header('Text Classifier')
+input = st.text_area("Please enter the text", value="")
+vec = vector.transform([input]).toarray()
+if st.button("Predict"):
+   
+    st.write(str(list(naivebayes.predict(vec))[0]).replace('0', 'TECH').replace('1', 'BUSINESS').replace('2', 'SPORTS').replace('3','ENTERTAINMENT').replace('4','POLITICS'))
